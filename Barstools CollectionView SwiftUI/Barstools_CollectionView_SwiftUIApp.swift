2f540c88ffc9434d80bae1f3c129c0b618c0d618
dlee2585 <95252619+dlee2585@users.noreply.github.com>
2022-01-05 16:23:16 -0500

    Initial Commit

---
+++ Barstools CollectionView SwiftUI/Barstools_CollectionView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barstools_CollectionView_SwiftUIApp.swift
+//  Barstools CollectionView SwiftUI
+//
+//  Created by David Lee on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Barstools_CollectionView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
